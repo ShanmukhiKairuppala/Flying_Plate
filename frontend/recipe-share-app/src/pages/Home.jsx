4b7691fa94a62f100e691c7c8b99a00154f61ec7
Krushna Varshitha <kkrushna185@gmail.com>
2025-02-02 18:31:41 +0530

    Initial commit of website files

---
+++ frontend/recipe-share-app/src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import foodRecipe from '../assets/food_photo.jpg'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import RecipeItems from '../components/RecipeItems'
+import { useNavigate } from 'react-router-dom'
+import Modal from '../components/Modal'
+import InputForm from '../components/InputForm'
+
+export default function Home() {
+    const navigate = useNavigate()
+    const [isOpen, setIsOpen] = useState(false)
+
+    const addRecipe = () => {
+        let token = localStorage.getItem("token")
+        if (token)
+            navigate("/addRecipe")
+        else {
+            setIsOpen(true)
+        }
+    }
+
+    return (
+        <>
+            <section className='home'>
+                <div className='left'>
+                    <h1>Share & View Food Recipes</h1>
+                    <h4>Welcome to Flying Plate, your passport to a world of culinary inspiration!  Whether you're a seasoned chef or just starting your cooking journey, our vibrant community is here to help you discover delicious recipes, share your own culinary creations, and connect with fellow food enthusiasts.  Explore a diverse collection of dishes, from comforting classics to exciting new flavors, all shared with passion and a sprinkle of creativity.  Get ready to ignite your taste buds and embark on a flavorful adventure with Flying Plate!</h4>
+                    <br></br>
+                    <button onClick={addRecipe}>Share your recipe</button>
+                </div>
+                <div className='right'>
+                    <img src={foodRecipe} width="320px" height="300px"></img>
+                </div>
+            </section>
+            <div className='bg'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#29424d" fillOpacity="1" d="M0,32L40,32C80,32,160,32,240,58.7C320,85,400,139,480,149.3C560,160,640,128,720,101.3C800,75,880,53,960,80C1040,107,1120,181,1200,213.3C1280,245,1360,235,1400,229.3L1440,224L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+            </div>
+            {(isOpen) && <Modal onClose={() => setIsOpen(false)}><InputForm setIsOpen={() => setIsOpen(false)} /></Modal>}
+            <div className='recipe'>
+                <RecipeItems />
+            </div>
+        </>
+    )
+}
